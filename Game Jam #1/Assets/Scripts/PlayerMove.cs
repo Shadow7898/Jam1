e5eaab2947ce22a98333f72b460d2e6d993f1431
Shadow7898 <45573393+Shadow7898@users.noreply.github.com>
2021-02-14 16:39:38 -0700

    Code update
    Camera zoom out
    Collectibles code implemented
    Basic GUI created

--- Game Jam #1/Assets/Scripts/PlayerMove.cs
+++ Game Jam #1/Assets/Scripts/PlayerMove.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerMove : MonoBehaviour
 {
+    public TextMeshProUGUI countText;
+    
     public float speed = 5.0f;
     private Rigidbody body;
+
+    private int count;
     void Start()
     {
+        count = 0;
         body = GetComponent<Rigidbody>();
+
+        SetCountText();
     }
 
     // Update is called once per frame
@@ -18,5 +26,21 @@ public class PlayerMove : MonoBehaviour
         float zforce = Input.GetAxis("Vertical");
 
         body.AddForce(new Vector3(xforce * speed, 0f, zforce * speed));
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("PickUp")){
+            other.gameObject.SetActive(false);
+
+            // Add one to the score variable 'count'
+			count = count + 100;
+
+			// Run the 'SetCountText()' function (see below)
+			SetCountText();
+        }
+    }
+
+    void SetCountText(){
+        countText.text = "Score: " + count.ToString();
     }
 }
